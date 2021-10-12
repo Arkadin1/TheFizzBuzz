f83ce69192c55d3ca26b67afd87efaa9e20c2e9e
Arkadiusz K <arkadini@live.com>
2021-10-12 17:50:35 +0200

    Dodaj pliki projektów.

---
+++ FizzBuzz.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheFizzBuzz
+{
+    public  class FizzBuzz
+    {
+
+        public  string WhyNumber(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                return "FizzBuzz";        
+            }
+            else if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+
+            return number.ToString();
+
+           
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheFizzBuzz
+{
+    class Program
+    {
+         public static void Main()
+        {
+            while (true)
+            { 
+
+                Console.WriteLine("Podaj liczbę: ");
+                var FizzBuzz = new FizzBuzz();
+                var number = CheckNumber();
+                FizzBuzz.WhyNumber(number);
+                Console.WriteLine(FizzBuzz.WhyNumber(number));
+            }
+          
+        }
+
+
+        private static int CheckNumber()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int Number))
+                {
+                    Console.WriteLine("Wpisałeś błędną liczbę!");
+                 
+                    continue;
+                }
+                else
+                {
+                    return Number;
+                }
+            }
+           
+        }
+    }
+
+}
